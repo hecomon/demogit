d5a26bb3501e7fcf87c6dad16ccf832421a0f0ba
Herlan Condori M <cmrhlner@gmx.com>
2025-10-31 23:03:34 -0400

    Mi primer commit

---
+++ PHPDocument1.php
@@ -0,0 +1,14 @@
+<?php
+function showHola(){ ?>
+	<p><?php echo 'Hola mundo'; ?></p>	
+<?php } ?>
+<html>
+<title>Hola Mundo</title>
+<meta charset='utf-8' />
+</head>
+
+<body>
+<p>PRUEBA DE HOLA MUNDO DESDE ZEND</p>
+<?php showHola(); ?>
+</body>
+</html>
